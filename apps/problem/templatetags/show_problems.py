787d15b0b97fcec2668ed29290429c4b9cf0785a
Kenny Meyer <knny.myer@gmail.com>
2011-06-25 15:16:48 -0400

    Add unit test for template tag helper function

--- apps/problem/templatetags/show_problems.py
+++ apps/problem/templatetags/show_problems.py
@@ -1,33 +1,38 @@
 from django import template
 from submission.models import Submission
 
-register = template.Library()
 
-@register.inclusion_tag('problem/templatetags/show_problems.html')
-def show_problems(participant):
-    """ Return all problems for the `participant` """
+def get_problem_meta_for(participant):
+    """ Return all problems with a submission from the `participant`.
+
+    @return dict
+    """
     problems = participant.competition.problem_set.all()
 
     problem_meta = []
     for problem in problems:
-        new_dict = {"solved": False,
-                    "submissions": 0,
+        new_dict = {"submissions": 0,
                     "problem": None,
                     "submission": None}
         for submission in problem.submission_set.filter(participant=participant):
             # Add a submission
             new_dict["submissions"] += 1
-            #if submission.result_correct():
-            #    new_dict["solved"] = True
-            #    # Add problem to dict
             new_dict["submission"] = submission
         new_dict["problem"] = problem
         problem_meta.append(new_dict)
+    
+    return problem_meta
+       
+
+register = template.Library()
+
+@register.inclusion_tag('problem/templatetags/show_problems.html')
+def show_problems(participant):
+    """ Return all problems for the `participant` """
 
     context = {
-        "problems": problem_meta,
+        "problems": get_problem_meta_for(participant),
         "participant": participant
     }
 
     return context
-
--- apps/problem/tests.py
+++ apps/problem/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
 
 from competition.models import Competition
+from participant.models import Participant
 from problem.models import Problem
+from submission.models import Submission
 
+from problem.templatetags.show_problems import get_problem_meta_for
 
 class ProblemTestCase(TestCase):
     """ Tests for Problem model. """
@@ -34,3 +37,23 @@ class ProblemTestCase(TestCase):
         url = self.problem.submit_url()
 
         self.assertEquals(url, u'/problem/%i/submit' % self.problem.id)
+
+
+class TemplateTagTestCase(TestCase):
+    """ Tests for template tags. """
+    fixtures = ["test_data.json"]
+
+    def setUp(self):
+        self.participant = Participant.objects.get(pk=1)
+
+    def test_get_problem_meta(self):
+        """ Test get_problem_meta_for helper function. """
+        problem_meta = get_problem_meta_for(self.participant)
+
+        problem = Problem.objects.get(pk=1)
+        submission = Submission.objects.get(problem=problem)
+
+        self.assertEquals(problem_meta,
+                          [{"submissions": 1,
+                            "problem": problem,
+                            "submission": submission}])
